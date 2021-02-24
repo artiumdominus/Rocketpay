f92423a61a0c1e0c0300573b5a429867b410ce73
Pedro Basilio <artiumdominus@protonmail.com>
2021-02-23 23:16:19 -0300

    add create user

---
+++ lib/rocketpay_web/views/users_view.ex
@@ -0,0 +1,13 @@
+defmodule RocketpayWeb.UsersView do
+
+  def render("create.json", %{user: user}) do
+    %{
+      message: "User created",
+      user: %{
+        id: user.id,
+        name: user.name,
+        nickname: user.nickname
+      }
+    }
+  end
+end
